6b43d625c517eefa5e64d21a72d489e5125c07a8
longhuyang <l814223024@163.com>
2022-04-23 03:10:25 +0800

    fix: 第五次

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      F222or11114444444444444111 a guide and recipes on how to configure / customize this project,<br>
+      F222or1111444444224444444111 a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
